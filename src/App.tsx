f78b814b19ba3b8c4405e77f8e724a1052b55d80
Luigi Matias <36388765+igiboi@users.noreply.github.com>
2022-01-20 09:37:39 -0500

    Added a sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
-import { createTheme, ThemeProvider, CssBaseline, TextField } from "@mui/material";
-import { AppBarComponent } from "./components";
+import {
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  TextField,
+} from "@mui/material";
+import { AppBarComponent, SideBar } from "./components";
 
 export const App: React.FC = (): JSX.Element => {
   const [themeMode, setThemeMode] = useState<"light" | "dark">("dark");
+  const [sideBarToggle, setSideBarToggle] = useState<boolean>(false);
 
   const theme = createTheme({
     palette: {
@@ -16,15 +22,24 @@ export const App: React.FC = (): JSX.Element => {
 
   const handleDrawerToggle = React.useCallback(() => {
     // toggle drawer here
-  }, [])
+    setSideBarToggle(!sideBarToggle);
+  }, [sideBarToggle]);
+
+  const onThemeChange = React.useCallback(() => {
+    setThemeMode(themeMode === "dark" ? "light" : "dark");
+  }, [themeMode]);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-        <AppBarComponent handleDrawerToggle={handleDrawerToggle} />
+      <AppBarComponent
+        handleDrawerToggle={handleDrawerToggle}
+        onThemeChange={onThemeChange}
+        isDarkMode={themeMode === "dark"}
+      />
+      <SideBar isOpen={sideBarToggle} handleDrawerToggle={handleDrawerToggle} />
     </ThemeProvider>
   );
 };
-
 
 export default App;
--- src/components/Appbar/AppBarComponent.tsx
+++ src/components/Appbar/AppBarComponent.tsx
@@ -1,25 +1,40 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
-
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Switch as ThemeSwitch,
+} from "@mui/material";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import MenuIcon from "@mui/icons-material/Menu";
 interface Props {
   handleDrawerToggle: () => void;
+  onThemeChange: () => void;
+  isDarkMode: boolean;
+  // isDrawerOpen: boolean;
 }
 
 export const AppBarComponent: React.FC<Props> = ({
   handleDrawerToggle,
+  onThemeChange,
+  isDarkMode,
+  // isDrawerOpen,
 }): JSX.Element => {
   return (
     <div>
-      <AppBar position="static" variant="elevation">
+      <AppBar position="static" variant="elevation" className="appbar-wrapper">
         <Toolbar variant="dense">
-          <IconButton
-            color="inherit"
-            onClick={handleDrawerToggle}
-            edge="start"
-          ></IconButton>
-          <Typography variant="subtitle1" component="div">
+          <IconButton color="inherit" onClick={handleDrawerToggle} edge="start">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="subtitle1" component="div" sx={{ flexGrow: 1 }}>
             React Repo Trading App
           </Typography>
+          {isDarkMode ? <DarkModeIcon /> : null}
+          <ThemeSwitch size="small" onChange={onThemeChange} color="default" />
+          {!isDarkMode ? <LightModeIcon /> : null}
         </Toolbar>
       </AppBar>
     </div>
